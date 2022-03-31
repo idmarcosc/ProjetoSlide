2708e2b67ca55d5f4f2de608919a1cf793e8ebaf
idmarcosc <92194475+idmarcosc@users.noreply.github.com>
2022-03-31 00:20:03 -0300

    Slide

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+let contador = 1;
+
+function verifica(){
+  let cont = 0;
+  if(document.getElementById('slide1').checked){
+    cont = 1
+  }if(document.getElementById('slide2').checked){
+    cont = 2
+  }if(document.getElementById('slide3').checked){
+    cont = 3
+  }if(document.getElementById('slide4').checked){
+    cont = 4
+  }if(document.getElementById('slide5').checked){
+    cont = 5
+  }
+  return cont
+}
+
+
+setInterval(() => {
+  contador = verifica() + 1;
+  if( contador > 5) {
+    contador = 1;
+  }
+  document.getElementById('slide' + contador).checked = true;
+
+}, 3000);
